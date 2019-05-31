6fc931a5a7522d6abd6cbc8dd982ba8524597426
Alzheimer10 <carlosanselmi2@gmail.com>
2019-05-31 19:31:39 -0400

    fix bugs htaccess

--- app/Http/Middleware/CheckForMaintenanceMode.php
+++ app/Http/Middleware/CheckForMaintenanceMode.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Foundation\Application;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode as Middleware;
 
 class CheckForMaintenanceMode extends Middleware
 {
+
+    protected $request;
+    protected $app;
+ 
+    public function __construct(Application $app, Request $request)
+    {
+        $this->app = $app;
+        $this->request = $request;
+    }
+
     /**
      * The URIs that should be reachable while maintenance mode is enabled.
      *
@@ -14,4 +28,14 @@ class CheckForMaintenanceMode extends Middleware
     protected $except = [
         //
     ];
+
+    public function handle($request, Closure $next)
+    {
+        if ($this->app->isDownForMaintenance())
+        {
+        	return response()->make(view('errors.503'), 500);
+        }
+ 
+        return $next($request);
+    }
 }
